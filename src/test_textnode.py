8c38bcd0c2d5a1dcfc8419cc48f2d6d67c48c148
ktanen <kevinbtanen@gmail.com>
2026-03-09 15:27:29 -0400

    Added a function to convert text nodes to HTML nodes

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -19,5 +19,38 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node.", TextType.LINK, "https://www.boot.dev")
         self.assertNotEqual(node, node2)
 
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_text_node_to_html_node_link(self):
+        # 1. Create a TextNode with a link type and a URL
+        node = TextNode("Click me!", TextType.LINK, "https://www.boot.dev")
+        
+        # 2. Convert it using your new function
+        html_node = text_node_to_html_node(node)
+        
+        # 3. Assert the details match a LeafNode <a> tag
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Click me!")
+        self.assertEqual(html_node.props, {"href": "https://www.boot.dev"})
+
+    def test_text_node_to_html_node_invalid(self):
+        # Create a node with a type that isn't in your TextType enum 
+        # (or just a plain string if your function doesn't strictly check the enum)
+        node = TextNode("Bad node", "not_a_type")
+        
+        # This checks that calling your function raises an Exception
+        with self.assertRaises(Exception):
+            text_node_to_html_node(node)
+
+    def test_image(self):
+        node = TextNode("Test",TextType.IMAGE,"https://www.example.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.props, {"src": "https://www.example.com", "alt": "Test"})
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     TEXT = "text"
@@ -19,3 +20,35 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            node_text = text_node.text
+            node = LeafNode(None,node_text)
+            return node
+        case TextType.BOLD:
+            node_text = text_node.text
+            node = LeafNode("b", node_text)
+            return node
+        case TextType.ITALIC:
+            node_text = text_node.text
+            node = LeafNode("i", node_text)
+            return node
+        case TextType.CODE:
+            node_text = text_node.text
+            node = LeafNode("code", node_text)
+            return node
+        case TextType.LINK:
+            node_text = text_node.text
+            url = text_node.url
+            props = {"href": url}
+            node = LeafNode("a", node_text, props)
+            return node
+        case TextType.IMAGE:
+            props = {"src": text_node.url, "alt": text_node.text}
+            node = LeafNode("img", "", props)
+            return node
+        case _:
+            raise Exception("invalid text type")
