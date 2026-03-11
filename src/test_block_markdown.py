84038d6c7a563af4296398106239a3bd2539bfc7
ktanen <kevinbtanen@gmail.com>
2026-03-11 09:24:54 -0400

    Added extra markdown_to_blocks tests for completeness

--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -22,5 +22,44 @@ This is the same paragraph on a new line
             ],
         )
 
+    def test_extra_spaces(self):
+        md = """
+This is **bolded** paragraph
+
+
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
+
+    def test_markdown_to_blocks_strip(self):
+        md = """
+    This is **bolded** paragraph   
+
+    This is another paragraph   
+    """
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph",
+            ],
+        )
+
 if __name__ == "__main__":
     unittest.main()
