4cbe8ae39f338cbfb88cc37e7738c73448bb3fa7
ktanen <kevinbtanen@gmail.com>
2026-03-12 12:22:27 -0400

    added more webpages and the ability to generate them recursively

--- src/main.py
+++ src/main.py
@@ -2,14 +2,19 @@ import os
 import os.path
 import shutil
 from copystatic import copy_files_recursive
-from page_generator import generate_page
+from page_generator import generate_pages_recursive
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 def main():
     if os.path.exists("public"):
         shutil.rmtree("public")
     copy_files_recursive("static", "public")
 
-    generate_page("content/index.md","template.html","public/index.html")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
 
 main()
--- src/page_generator.py
+++ src/page_generator.py
@@ -2,6 +2,7 @@ import os
 import os.path
 from extract_title import extract_title
 from block_markdown import markdown_to_html_node
+from pathlib import Path
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generatiing page from {from_path} to {dest_path} using {template_path}")
@@ -25,3 +26,17 @@ def generate_page(from_path, template_path, dest_path):
 
     with open(dest_path, "w") as f:
         f.write(full_html)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    full_content_path = Path(dir_path_content)
+    full_template_path = Path(template_path)
+    full_dest_dir_path = Path(dest_dir_path)
+
+    for entry in full_content_path.iterdir():
+        if entry.is_file() and str(entry).endswith(".md"):
+            full_dest_dir_path.mkdir(parents=True, exist_ok=True)
+            final_filepath = (full_dest_dir_path / entry.name).with_suffix(".html")
+            
+            generate_page(entry, full_template_path, final_filepath)
+        elif entry.is_dir():
+            generate_pages_recursive(entry, full_template_path, full_dest_dir_path / entry.name)
