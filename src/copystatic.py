8e0c4df567a4eef34e97af0ff08682f448836d75
ktanen <kevinbtanen@gmail.com>
2026-03-12 09:34:34 -0400

    moved copy logic to its own module

---
+++ src/copystatic.py
@@ -0,0 +1,15 @@
+import os
+import os.path
+import shutil
+
+def copy_files_recursive(source, dest):
+    if not os.path.exists(dest):
+        os.mkdir(dest)
+    for entry in os.listdir(source):
+        from_path = os.path.join(source,entry)
+        dest_path = os.path.join(dest, entry)
+        print(f" * {from_path} -> {dest_path}")
+        if os.path.isfile(from_path):
+            shutil.copy(from_path,dest_path)
+        else:
+            copy_files_recursive(from_path, dest_path)
