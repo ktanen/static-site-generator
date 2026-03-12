99319f33d064542d3be1545d0e4fa79db1149596
ktanen <kevinbtanen@gmail.com>
2026-03-12 09:30:58 -0400

    copies files from a source to a destination

--- src/main.py
+++ src/main.py
@@ -1,7 +1,24 @@
-from textnode import TextNode, TextType
+import os
+import os.path
+import shutil
+
+
+def copy_files_recursive(source, dest):
+    if not os.path.exists(dest):
+        os.mkdir(dest)
+    for entry in os.listdir(source):
+        from_path = os.path.join(source,entry)
+        dest_path = os.path.join(dest, entry)
+        print(f" * {from_path} -> {dest_path}")
+        if os.path.isfile(from_path):
+            shutil.copy(from_path,dest_path)
+        else:
+            copy_files_recursive(from_path, dest_path)
+
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+    copy_files_recursive("static", "public")
 
 main()
